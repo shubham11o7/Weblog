492bdd277ad973c92068c63f3312e9e4c8267825
Shubham <sdeshmukh440@gmail.com>
2020-12-07 09:31:07 +0530

    Created Forms for Adding Topics and aslo written related test Cases

--- weblog/urls.py
+++ weblog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 
 ]
